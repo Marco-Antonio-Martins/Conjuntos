36382eb25b7af65e9b1ea755d52ad7c625b817d8
Marco Antônio <marcomartins06@rede.ulbra.br>
2022-09-12 23:57:46 -0300

    arrumando o código

--- Conjunto.py
+++ Conjunto.py
@@ -1,59 +1,61 @@
-class Conjunto(): #criando a classe conjunto
-  elementos = []
-  
-  def __init__(self, elementos=[]):
-    self.elementos = elementos
+class Conjunto():  # criando a classe conjunto
+    elementos: list = []
 
-  def verElementos(self):
-    if len(self.elementos) > 0:
-      ret="{"
-      for elemento in self.elementos:
-        ret+=str(elemento)+ ","
-      ret=ret[:-1]
-      ret+="}"
-      print(ret)
-    else:
-      print("{} - CONJUNTO VAZIO")
+    def __init__(self, elementos=[]) -> None:
+        self.elementos = elementos
 
-  def possui(self, elemento): #Pertinência
-    if elemento in self.elementos:
+    def verElementos(self) -> None:
+        if len(self.elementos) > 0:
+            ret = "{"
+            for elemento in self.elementos:
+                ret += str(elemento) + ","
+            ret = ret[:-1]
+            ret += "}"
+            print(ret)
+        else:
+            print("{} - CONJUNTO VAZIO")
+
+    def possui(self, elemento) -> bool:  # Pertinência
+        if elemento in self.elementos:
+            return True
+        return False
+
+    def isSubconjunto(self, conjunto) -> bool:  # Subconjunto
+        elementos = conjunto.elementos
+
+        for elemento in self.elementos:
+            if elemento not in elementos:
+                return False
         return True
-    return False
 
-  def isSubconjunto(self, conjunto):
-    elementos = conjunto.elementos
+    def isEqual(self, conjunto) -> bool:  # Igualdade
+        if self.isSubconjunto(conjunto) and conjunto.isSubconjunto(self):
+            return True
+        return False
 
-    for elemento in self.elementos:
-      if elemento not in elementos:
-        return False      
-    return True
 
-  def isEqual(self, conjunto):
-    if self.isSubconjunto(conjunto) and conjunto.isSubconjunto(self):
-      return True
-    return False
+def uniao(a, b) -> Conjunto:  # União
+    retornoUniao = []
 
-def uniao(a,b):
-  retornoUniao = []
+    for elemento in a.elementos:
+        if elemento not in retornoUniao:
+            retornoUniao.append(elemento)
 
-  for elemento in a.elementos:
-    if elemento not in retornoUniao:
-      retornoUniao.append(elemento)
+    for elemento in b.elementos:
+        if elemento not in retornoUniao:
+            retornoUniao.append(elemento)
 
-  for elemento in b.elementos:
-    if elemento not in retornoUniao:
-      retornoUniao.append(elemento)
+    ret = Conjunto(retornoUniao)
+    return ret
 
-  ret = Conjunto(retornoUniao)
-  return ret
 
-def intersecao(a,b):
-  retornoIntersecao = []
-  for elemento in a.elementos:
-    if elemento in b.elementos:
-      retornoIntersecao.append(elemento)
-  ret = Conjunto(retornoIntersecao)
-  return ret
+def intersecao(a, b) -> Conjunto:  # Interseção
+    retornoIntersecao = []
+    for elemento in a.elementos:
+        if elemento in b.elementos:
+            retornoIntersecao.append(elemento)
+    ret = Conjunto(retornoIntersecao)
+    return ret
 
 # FALTA CRIAR AS FUNÇÕES:
 # c) diferença entre dois conjuntos A e B
--- usaConjunto.py
+++ usaConjunto.py
@@ -1,33 +1,64 @@
 from Conjunto import *
 # CRIANDO CONJUNTOS
-A = Conjunto([7,8])
-B = Conjunto([0,1,2])
-C = Conjunto([2,1,0])
+A = Conjunto([7, 8])
+B = Conjunto([0, 1, 2])
+C = Conjunto([2, 1, 0, 3])
+D = Conjunto()
+E = Conjunto([7, 8])
 
-print("-"*50)
-# PRINTANDO ELEMENTOS DE CADA CONJUNTO
-print('Elementos de A')
-A.verElementos()
-print('Elementos de B')
-B.verElementos()
-print('Elementos de C')
-C.verElementos()
 
-print("-"*50)
-# Verificando funções
-print("O Conjunto A possui o elemento 2? {}".format(A.possui(2)))
-print("O Conjunto A é subconjunto de B? {}".format(A.isSubconjunto(B))) #verificar se A é subCJ de A (igualdade)
-print("O Conjunto B é subconjunto de A? {}".format(B.isSubconjunto(A)))
-print("O Conjunto A é igual ao B? {}".format(A.isEqual(B)))
-print("O Conjunto B é igual ao C? {}".format(B.isEqual(C)))
+def testaVerElementos():
+    A.verElementos()
+    B.verElementos()
+    C.verElementos()
+    D.verElementos()
+    E.verElementos()
+    print('-'*50)
 
-print("-"*50)
-#UNIÃO DO CONJUNTO A E B
-print("A união dos conjuntos A e B é:")
-uniao(A,B).verElementos()
 
-print("-"*50)
-# INTERSEÇÃO DO CONJUNTO A E B
-print("A interseção dos conjuntos A e B é:")
-intersecao(A,B).verElementos()
-print("-"*50)
+def testaPertinencia():
+    print(f"O elemento 7 pertence ao conjunto A? {A.possui(7)}")
+    print(f"O elemento 0 pertence ao conjunto B? {B.possui(0)}")
+    print(f"O elemento 5 pertence ao conjunto D? {D.possui(5)}")
+    print('-'*50)
+
+
+def testaSubconjuntos():
+    print(f'A é subconjunto de B? {A.isSubconjunto(B)}')
+    print(f'B é subconjunto de C? {B.isSubconjunto(C)}')
+    print(f'C é subconjunto de B? {C.isSubconjunto(B)}')
+    print('-'*50)
+
+
+def testaIgualdade():
+    print(f'O Conjunto A é igual ao E? {A.isEqual(E)}')
+    print(f'O Conjunto A é igual ao B? {A.isEqual(B)}')
+    print('-'*50)
+
+
+def testaUniao():
+    print('Conjunto A')
+    A.verElementos()
+    print('Conjunto B')
+    B.verElementos()
+    print('A união entre o Conjunto A e o Conjunto B é')
+    uniao(A, B).verElementos()
+    print('-'*50)
+
+
+def testaIntersecao():
+    print('Conjunto B')
+    B.verElementos()
+    print('Conjunto C')
+    C.verElementos()
+    print('A intersecao entre o Conjunto B e o Conjunto C é')
+    intersecao(B, C).verElementos()
+    print('-'*50)
+
+
+testaVerElementos()
+testaPertinencia()
+testaSubconjuntos()
+testaIgualdade()
+testaUniao()
+testaIntersecao()
